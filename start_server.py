90f9e836e734890576f1caf514750ddb2bac1172
Achille Pisani <achillepisa@gmail.com>
2025-07-18 00:00:10 +0200

    Update start_server.py

--- start_server.py
+++ start_server.py
@@ -7,12 +7,12 @@ import os
 
 PORT = 8000
 
-def run_server()
+def run_server():
     os.chdir(os.path.dirname(__file__))  # Serve dalla directory dello script
     handler = http.server.SimpleHTTPRequestHandler
-    with socketserver.TCPServer((, PORT), handler) as httpd
-        print(f🚀 Serving at httplocalhost{PORT})
-        webbrowser.open(fhttplocalhost{PORT})
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print(f"Serving at http://localhost:{PORT}")
+        webbrowser.open(f"http://localhost:{PORT}")
         httpd.serve_forever()
 
 thread = threading.Thread(target=run_server)
